9d8ce96ab5e219adcade0b0ee0db309f24471e5a
MaudBeeckmans <73498415+MaudBeeckmans@users.noreply.github.com>
2020-11-24 20:35:14 +0100

    Visibility_check_opacity
    * 1 function to be imported from another file 
    * big grating orientation = 0; target_orientation = 90
    * target: locations on x-axis fixed to the middle of the white stripes of the grating to make sure that the visibility of the target is the same at each trial 
    * manipulation: opacity is the variable that will be manipulated in questhandlers & thus the variable of importance for the target_visibility

--- Target_Visibility_check.py
+++ Target_Visibility_check.py
@@ -17,42 +17,28 @@ Some information:
 import numpy as np
 import math, time, os, pandas
 from psychopy import visual, data, core, event, gui
-
+from Target_positions_function import create_target_positions
 
 
 #B. Create the fixation point with function                                     Fixation point
 #create the fixation point              #check whether the variables are important (maybe for formula)
 
-win = visual.Window(fullscr = True, monitor = 'Laptop', units = 'deg')
+win = visual.Window(size = [800, 600], monitor = 'Laptop', units = 'deg')
 
-# E. Create the Target                                                          Target
-def create_target_positions(size = None): 
-    r = 1.5
-    target_left_positions = np.empty([int(size/2), 2])
-    target_right_positions = np.empty([int(size/2), 2])
-    t = np.random.uniform(0, 1, size=size)
-    u = np.random.uniform(0, 1, size=size)
-    y = r*np.sqrt(t) * np.sin(2*np.pi*u)
-    x = r*np.sqrt(t) * np.cos(2*np.pi*u)
-    x = np.round(x, 1)
-    y = np.round(y, 1)
-    target_left_positions[:, 0] = x[0:int(size/2)] - 5
-    target_left_positions[:, 1] = y[0:int(size/2)]
-    target_right_positions[:, 0] = x[int(size/2):] + 5
-    target_right_positions[:, 1] = y[int(size/2):]
-    return target_left_positions, target_right_positions
 
-target_template =visual.GratingStim(win=win,tex='sin', mask='gauss',ori=0, sf=5, pos=(1,1), size= (1,1), interpolate=False)
+
+target_template =visual.GratingStim(win=win,tex='sin', mask='gauss',ori=90, sf=1.4, pos=(1,1), size= (1,1), interpolate=False, contrast = 1)
 left_count = 0
 right_count = 0
-def target_prepare(target_loc = None, amplitude = 25, left_i = left_count, right_i = right_count):         #target location: '0.0' = L, '1.0' = R
+def target_prepare(target_loc = None, contrast = 1, opacity = 1, left_i = left_count, right_i = right_count):         #target location: '0.0' = L, '1.0' = R
                                                                                                         #corresponds with trial['Target_hemifield']
     if target_loc == 0: 
         target_position = t_left_pos[left_i, :]
     else: 
         target_position = t_right_pos[right_i, :]
     target_template.pos = target_position
-    target_template.maskParams={'sd':amplitude/2}
+    target_template.opacity = opacity
+    target_template.contrast = contrast
 
 
 #C. Create a function for the gratings                                 Drifting gratings
@@ -60,17 +46,38 @@ def target_prepare(target_loc = None, amplitude = 25, left_i = left_count, right
 d_grating = 4
 sf_grating = 1.4
 pos_grating = np.array([[-5, 0], [5,0]])
-GratingA = visual.GratingStim(win, tex = 'sin', mask = 'circle', sf = sf_grating, size = d_grating, ori = 90, pos = pos_grating[0])
+GratingA = visual.GratingStim(win, tex = 'sin', mask = 'circle', sf = sf_grating, size = d_grating, ori = 0, pos = pos_grating[0])
 GratingB = visual.GratingStim(win, tex = 'sin', mask = 'circle', sf = sf_grating, size = d_grating, ori = 0, pos = pos_grating[1])
+
+target2 = visual.GratingStim(win=win,tex='sin', mask='gauss',ori=0, sf=1.4, pos=(-4,0), size= (1,1), interpolate=False)
+target3 = visual.GratingStim(win=win,tex='sin', mask='gauss',ori=0, sf=1.4, pos=(-6,0), size= (1,1), interpolate=False)
+
+test_opa = 0.5
+test_contr = 1
+target2.opacity = test_opa
 
 
 t_left_pos, t_right_pos = create_target_positions(size = 84)
 
-for i in range(t_left_pos.shape[0]): 
-    target_prepare(target_loc = 0, amplitude = 30, left_i = i, right_i = None)
+for i in range(int(84/2)): 
+    target_prepare(target_loc = 1, opacity = test_opa, contrast = test_contr, left_i = i, right_i = i)
+    
+    #target_template.pos = possible_loc[i]
+    #target2.pos = t_left_pos[i+1]
+    
     GratingA.draw()
+    GratingB.draw()
     target_template.draw()
     win.flip()
+#    for Frame in range(40): 
+#        GratingA.draw()
+#        GratingB.draw()
+#        if Frame == 36 or Frame == 37: 
+#            target_template.draw()
+        #target2.draw()
+    #    target2.draw()
+    #    target3.draw()
+        #win.flip()
     
     response = event.waitKeys(keyList = ['f', 'j', 'esc', 'escape'])
     if response[0] == 'f': 
@@ -79,7 +86,4 @@ for i in range(t_left_pos.shape[0]):
         break 
     else: 
         print('target NOT located at {}')
-
-
-win.close()
 
